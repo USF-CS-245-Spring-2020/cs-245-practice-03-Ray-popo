0fda5fe3be0dfcd3e0d529effccbb7a2b3a1982c
Ray-popo <Ma3092353>
2020-02-19 22:41:28 -0800

    assignment3

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,20 +1,20 @@
 public class InsertionSort implements Practice03Sort
 {
-    public void sort(int [] a)
-    {
-        int l = a.length;
-        for (int i = 1; i < l; i++)
-        {
-            int piovt = a[i];
-            int j = i-1;
-            //move the element, if the element greater than target
-            //so move the element ahead to the "current" position
-            while ((j>-1) && (a[j]>piovt))
-            {
-                a[j+1] = a[i];
-                j--;
+    public void sort(int [] a) {
+        for (int i = 1; i < a.length; i++) {
+            int index = i;
+            for (int j = i - 1; j >= 0; j--) {
+                if (a[j] > a[index]) {
+                    swap(a, j, index);
+                    index = j;
+                }
             }
-            a[j+1] = piovt;
         }
+    }
+    public void swap(int [] a , int n, int m)
+    {
+        int temp = a [m];
+        a[m]= a[n];
+        a[n]= temp;
     }
 }
